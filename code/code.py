a178c0347f52b708f9c446cdcef35f063e8e0d37
Eric Lee <saintgimp@hotmail.com>
2020-11-15 14:59:07 -0800

    Updated code

--- code/code.py
+++ code/code.py
@@ -11,6 +11,13 @@ first_line_y = 3
 line_height = 11
 line_glyphs = 63
 
+# These voltage ranges assume that we have minimal voltage
+# drop between the battery terminals and the monitor
+battery_voltage_green = 12.2
+battery_voltage_yellow = 11.9
+battery_voltage_red = 11.7
+battery_voltage_cutoff = 11.5
+
 i2c = board.I2C()
 
 # With these settings, the readings will update every 1055 ms
@@ -52,7 +59,14 @@ red_led = DigitalInOut(board.D5)
 red_led.direction = Direction.OUTPUT
 red_led.value = False
 
+# We'd like to turn off the RGB led on the board, which is currently
+# defaulting to white, but that's not possible yet (https://github.com/adafruit/circuitpython/issues/2707)
+
+#line1.text = "Saint Gimp\nBattery monitor"
+#time.sleep(1.055)
+
 voltage_low_point = 1000
+amp_hours = 0.0
 energy = 0.0
 then = time.monotonic()
 
@@ -61,27 +75,29 @@ while True:
 
     now = time.monotonic()
     elapsed_time = now - then
+    elapsed_hours = (elapsed_time / 60 / 60)
     then = now
 
     voltage = ina260.voltage
     current = ina260.current / 1000
     power = ina260.power / 1000
-    energy = energy + (power * (elapsed_time / 60 / 60))
-
-    line1.text = "{:.2f}".format(voltage) + " V " + "{:.2f}".format(current) + " A\n" + "{:.2f}".format(power) + " W\n" + "{:.2f}".format(energy) + " watt-hours"
+    amp_hours = amp_hours + (current * elapsed_hours)
+    energy = energy + (power * elapsed_hours)
+    
+    line1.text = "{:.2f}".format(voltage) + " V  " + "{:.3f}".format(current) + " A\n" + "{:.2f}".format(power) + " W\n" + "{:.3f}".format(amp_hours) + " Ah  " + "{:.2f}".format(energy) + " Wh"
 
     if voltage < voltage_low_point:
         voltage_low_point = voltage
 
-    if voltage_low_point > 11.5:
+    if voltage_low_point > battery_voltage_green:
         green_led.value = True
         yellow_led.value = False
         red_led.value = False
-    elif voltage_low_point > 11.0:
+    elif voltage_low_point > battery_voltage_yellow:
         green_led.value = False
         yellow_led.value = True
         red_led.value = False
-    elif voltage_low_point > 10.5:
+    elif voltage_low_point > battery_voltage_red:
         green_led.value = False
         yellow_led.value = False
         red_led.value = True
@@ -90,9 +106,8 @@ while True:
         yellow_led.value = False
         red_led.value = False
 
-    if voltage_low_point > 10.5:
+    if voltage_low_point > battery_voltage_cutoff:
         power_enable.value = True
     else:
         power_enable.value = False
 
-    #print(voltage_low_point)
